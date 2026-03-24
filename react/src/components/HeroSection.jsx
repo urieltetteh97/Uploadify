7fe38403310cf574c75d6892d49c4792b9907b31
Uriel Tetteh <urt12597@yahoo.co.uk>
2026-03-24 23:28:54 +0000

    grid view adjusted to be single column on smaller screens

--- react/src/components/HeroSection.jsx
+++ react/src/components/HeroSection.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import HomeImg from '../assets/popp.png'
 
 function HeroSection() {
   return (
-    <div className='w-full min-h-screen grid grid-cols-2'>
+    <div className='w-full min-h-screen grid grid-cols-1 md:grid-cols-2'>
         <div className='w-full h-full flex flex-col items-start justify-center space-y-6 px-20'>
             <h1 className='text-4xl font-bold text-black'>Students Submit, Schools Review, Everyone Stays Informed</h1>
             
@@ -10,7 +11,7 @@ function HeroSection() {
             <button className='bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer'>Get Started</button>
         </div>
         <div className='w-full h-full flex items-center justify-center'>
-            <img src="https://via.placeholder.com/400x300" alt="Hero Image" className='w-full h-auto rounded-lg shadow-lg' />
+            <img src={HomeImg} alt="Hero Image" className='object-contain rounded-lg' />
         </div>
     </div>
   )
